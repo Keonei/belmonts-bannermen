ef9b34d4426dd306bc1a95af1787843a37deee2c
Keonei <designmonkey@gmail.com>
2019-11-20 20:16:34 -0800

    updated code, added blog, teams sections

--- scripts/script.js
+++ scripts/script.js
@@ -10,28 +10,27 @@ $(document).ready(function() {
 
 // scrollto animation //
 
+
 $(function() {
+
   $('a[href*="#"]:not([href="#"])').click(function() {
     var target = $(this.hash);
 
     target = target.length ? target : $('[name=' + this.hash.substr(1) + ']');
     if (target.length) {
       $('html,body').animate({
-        scrollTop: target.offset().top
+        scrollTop: target.offset().top -60
       }, 1000);
     }
   });
 });
 
-// scrollup animation //
 
-$(document).scroll(function() {
-  var y = $(this).scrollTop();
-  var x = jQuery('#main').offset().top;
-
-  if (y > x) {
-    $('.scrollUp, float-banner').fadeIn(200);
+function myFunction() {
+  var x = document.getElementById("navbar");
+  if (x.className === "navbar-nav") {
+    x.className += "responsive";
   } else {
-    $('.scrollUp, float-banner').fadeOut(200);
+    x.className = "navbar";
   }
-});
+}
